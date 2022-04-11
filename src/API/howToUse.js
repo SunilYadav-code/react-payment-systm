d544ef4f0870fe2f17361153155db0ff388071a6
SunilYadav-code <rocky.sunil97@gmail.com>
2022-04-11 12:17:19 +0530

    Add files via upload

---
+++ src/API/howToUse.js
@@ -0,0 +1,19 @@
+const howToUseApp = [
+    {
+      id: 1,
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, Etquia quis?Lorem ipsum,  Etquia quis?",
+    },
+    {
+      id: 2,
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 3,
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, consectetur adipisicing elit. Etquia quis?Lorem ipsum?",
+    },
+  ];
+  
+  export default howToUseApp;
---
+++ src/API/workApi.js
@@ -0,0 +1,22 @@
+const workapi = [
+    {
+      id: 1,
+      logo: "fas fa-download",
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    },
+    {
+      id: 2,
+      logo: "fas fa-chalkboard-teacher",
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    },
+    {
+      id: 3,
+      logo: "fas fa-donate",
+      title: "Medical",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    },
+  ];
+  
+  export default workapi;
